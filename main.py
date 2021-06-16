09879301f1e49915ddcc8a3ee202aa4faa07c203
Ritu Gala <39478730+ritugala@users.noreply.github.com>
2021-06-16 15:42:13 +0530

    Question Generated from Videos

---
+++ main.py
@@ -0,0 +1,51 @@
+import transformers
+from transcripts import *
+import nltk
+from pipelines import pipeline
+from answers_similarity import similarity
+
+
+def generate_text(text):
+	timestamped_text = {}
+	all_text = ""
+	split_text = text.split('\n')
+
+	for i in range(len(split_text) - 1):
+		if i % 2 == 0:
+			timestamped_text[split_text[i]] = split_text[i + 1]
+		else:
+			all_text += " " + split_text[i]
+
+	all_text = all_text.lstrip()
+	return timestamped_text, all_text
+
+
+def answer_similarity(model_answer, student_answer):
+	score = similarity(model_answer, student_answer)
+	if score < 0.33:
+		return "low similarity"
+	elif score < 0.66:
+		return "medium similarity"
+	else:
+		return "high similarity"
+
+
+timestamped_text, plain_text = generate_text(text18)
+
+nlp = pipeline("question-generation")
+
+all_questions = nlp()
+
+for question in all_questions:
+
+	print("Sentence: " + question["sentence"])
+	print("Question: " + question["question"])
+	print("Answer: " + question["answer"])
+	print("Index:" + str(question["index"]))
+
+	# Image labels and any additional labels provided by lecturer
+	if ("equation" in question["question"].lower() or "graph" in question["question"].lower()):
+		print("Question Can have image")
+		print("The screenshot of the video at this time is passed to the image detection model")
+
+
